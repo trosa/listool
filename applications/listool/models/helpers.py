a4367f68f2a257fb9acd0b97ed440085ba29413c
Tiago Rosa <tiago.strato@gmail.com>
2011-01-21 14:52:14 -0200

    Definidas funcoes de fetch dos dados

---
+++ applications/listool/models/helpers.py
@@ -0,0 +1,20 @@
+import urllib
+import string
+
+def get_courses():
+	url = 'http://www.vestibular.ufrgs.br/cv2011/DENSIDADE_2011.HTM'
+	sock = urllib.urlopen(url)
+	courses = []
+	for line in sock.readlines():
+		if line[:3] == '<B>' and line[5].isdigit():
+			courses.append(line.split('  ')[1].strip())
+	return courses
+
+def get_people(course_name, letter):
+	url = 'http://www.ufrgs.br/coperse/cv2010/listao/CV2010_%s.htm' % letter
+	people = []
+	sock = urllib.urlopen(url)
+	for line in sock.readlines():
+		if line.split('  ')[-1].strip() == course_name:
+			people.append(" ".join(line.split('  ')[0].split()[6:]))
+	return people
