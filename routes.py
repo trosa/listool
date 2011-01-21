3b5d78a56c7d36427744c59c216fea0b3201d85d
Tiago Rosa <tiago.strato@gmail.com>
2011-01-21 13:47:28 -0200

    Initial commit

---
+++ routes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# default_application, default_controller, default_function
+# are used when the respective element is missing from the
+# (possibly rewritten) incoming URL
+#
+default_application = 'listool'    # ordinarily set in base routes.py
+default_controller = 'default'  # ordinarily set in app-specific routes.py
+default_function = 'index'      # ordinarily set in app-specific routes.py
